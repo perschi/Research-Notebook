f154aa5666d8457cbe6552305ef0e31fa68692e2
jan-ole <jan-ole.perschewski@st.ovgu.de>
2020-12-13 19:36:12 +0100

    Added first prototype for cells Added Flask server dummy Added gitignore Added svelte template

---
+++ client/src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+import { each } from 'svelte/internal';
+
+   import Cell from './Cell.svelte';
+   
+   //let cells = [{id:'Undefined', type: 'Markdown', file: 'index.txt', text: 'Type here !', dirty: False}];
+   let cells = [{id:'Undefined',text:'1', type: 'Markdown', file: 'index.txt', dirty: false},
+    {id:'Undefined', text:'2', type: 'Code', file: 'index.txt', dirty: false}];
+
+   function addCell(){
+      cells = cells.concat({
+         id:"Undefined",
+         text:"New cell",
+         type:"Markdown",
+         file:"Index.txt",
+         dirty: true
+      });
+   }
+</script>
+
+<h1> Research-Journal </h1>
+
+<table style="width:100%">
+
+
+   <tr>
+    <th style="width:20%">Files</th>
+    <th style="width:80%">Cells</th>
+  </tr>
+  <tr>
+    <td>
+       <!--File Structure here -->
+    </td>
+    <td>
+       {#each cells as {text, id, type, file}}
+       <Cell bind:cell_id={id} bind:cell_type={type} bind:cell_file={file}>
+          {text}
+      </Cell>
+      {/each}
+      <button type="button" style="center" on:click={addCell}>New Cell</button> 
+   </td>
+  </tr>
+</table>
+
