63b8c024f1d4a67519b7224300b3ec30eb9bc85e
perschi <73707155+perschi@users.noreply.github.com>
2020-12-13 20:20:18 +0100

    added visual dummy for file tree

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,10 +2,26 @@
 import { each } from 'svelte/internal';
 
    import Cell from './Cell.svelte';
-   
-   //let cells = [{id:'Undefined', type: 'Markdown', file: 'index.txt', text: 'Type here !', dirty: False}];
+   import FileTree from './FileTree.svelte';
+
    let cells = [{id:'Undefined',text:'1', type: 'Markdown', file: 'index.txt', dirty: false},
     {id:'Undefined', text:'2', type: 'Code', file: 'index.txt', dirty: false}];
+
+    let file_tree = {
+       name:'directory',
+       depth: 0,
+       type: 'directory',
+       sub_directories:[
+          {name:'directory2',
+          type: 'directory',
+          depth:1,
+         sub_directories: []},
+         {name:'File1',
+         depth:1,
+         type:'file',
+         sub_directories: []}
+       ]
+    };
 
    function addCell(){
       cells = cells.concat({
@@ -28,8 +44,9 @@ import { each } from 'svelte/internal';
     <th style="width:80%">Cells</th>
   </tr>
   <tr>
-    <td>
+    <td style='vertical-align: top'>
        <!--File Structure here -->
+       <FileTree bind:file_tree={file_tree}/>
     </td>
     <td>
        {#each cells as {text, id, type, file}}
---
+++ client/src/FileTree.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let file_tree;
+    export let selected;
+
+    function drawTree(root, depth){
+        alert('test')
+    }
+</script>
+
+<style>
+    .highlight:hover {
+        background-color: lightblue;
+    }
+
+    .element {
+        border: 1px solid black;
+        background-color: white;
+        margin: 1px;
+    }
+
+    .container {
+        background-color: rgb(247, 247, 247);
+		border: 1px solid black;
+		margin: 2px;
+		padding: 4px;
+    }
+</style>
+
+<div class="container">
+    <div class="highlight element" > {file_tree.name} </div> 
+
+    {#each file_tree.sub_directories as {name, sub_directories, depth}}
+        <div class="highlight element" style="padding-left: {(depth + 1) * 4}px;"> {name} </div> 
+    {/each}
+
+</div>
+
